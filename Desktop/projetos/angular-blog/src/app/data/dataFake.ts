2c6fea54472a7ea02353012353c57f45570af378
Muniz94 <muniz91@hotmail.com>
2024-02-03 16:35:51 -0300

    Criei o conteúdo dinamicamente

---
+++ Desktop/projetos/angular-blog/src/app/data/dataFake.ts
@@ -0,0 +1,26 @@
+export const dataFake = [
+  {
+    "id": 1,
+    "title": "Filme da Liga da Justiça",
+    "description": "Super Homem se volta contra a Liga da Justiça.",
+    "photoCover": "https://s2.glbimg.com/Me4ahG03P33OZ7JKA_VEBBuHtI24spnQcIpwvkoZPloU3yYt3nKO6iKKXKNdl0MN/e.glbimg.com/og/ed/f/original/2013/07/12/790_pesonagem.jpg"
+  },
+  {
+    "id": 2,
+    "title": "Filme do Batman",
+    "description": "Batman Retorna!",
+    "photoCover": "https://t.ctcdn.com.br/Uv-cy0NNdHv6vfgyfzT1k9p2LX0=/640x360/smart/i567736.jpeg"
+  },
+  {
+    "id": 3,
+    "title": "Filme dos X-Men",
+    "description": "Os X-Men estão atrás do Magneto.",
+    "photoCover": "https://img.olhardigital.com.br/wp-content/uploads/2023/12/X-Men-via-Fox-reproducao.png"
+  },
+  {
+    "id": 4,
+    "title": "Filme do Wolverine",
+    "description": "Wolwerine enfrenta o Dentes de Sabre.",
+    "photoCover": "https://diariodonordeste.verdesmares.com.br/image/contentid/policy:1.3377855:1686042039/Wolverine.jpeg?f=default&$p$f=9ec6a63"
+  }
+]
